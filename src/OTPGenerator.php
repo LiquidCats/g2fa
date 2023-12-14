cafc8762d83290b666de30a1bc85320916689055
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-12-14 22:49:25 +0400

    chore: google 2fa support

---
+++ src/OTPGenerator.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LiquidCats\G2FA;
+
+use LiquidCats\G2FA\Contracts\Support\Validator;
+use LiquidCats\G2FA\Enums\Algorithm;
+use LiquidCats\G2FA\Exceptions\IncompatibleWithAuthenticatorException;
+use LiquidCats\G2FA\Exceptions\InvalidCharactersException;
+use LiquidCats\G2FA\Exceptions\SecretKeyTooShortException;
+use LiquidCats\G2FA\Support\SecretValidator;
+use ParagonIE\ConstantTime\Base32;
+use SensitiveParameter;
+use function hash_hmac;
+use function ord;
+use function pack;
+use function str_pad;
+use function strlen;
+use function strtoupper;
+use function substr;
+use function unpack;
+use const STR_PAD_LEFT;
+
+readonly class OTPGenerator
+{
+    public function __construct(
+        private Validator $validator = new SecretValidator(),
+        private Algorithm $algorithm = Algorithm::SHA512,
+        private int $oneTimePasswordLength = 6,
+    ) {
+    }
+
+    /**
+     * @param string $secret
+     * @param int    $counter
+     *
+     * @return string
+     *
+     * @throws IncompatibleWithAuthenticatorException
+     * @throws InvalidCharactersException
+     * @throws SecretKeyTooShortException
+     */
+    public function generate(#[SensitiveParameter] string $secret, int $counter): string
+    {
+        if (strlen($secret) < 8) {
+            throw new SecretKeyTooShortException();
+        }
+
+        $secret = $this->decodeSecret($secret);
+
+        return str_pad(
+            $this->oathTruncate($this->generateHotp($secret, $counter)),
+            $this->oneTimePasswordLength,
+            '0',
+            STR_PAD_LEFT
+        );
+    }
+
+    /**
+     * Decodes a secret.
+     *
+     * @param string $secret The secret to be decoded. (SensitiveParameter)
+     *
+     * @return string The decoded secret.
+     *
+     * @throws IncompatibleWithAuthenticatorException
+     * @throws InvalidCharactersException
+     * @throws SecretKeyTooShortException
+     */
+    private function decodeSecret(#[SensitiveParameter] string $secret): string
+    {
+        $secret = strtoupper($secret);
+
+        $this->validator->validate($secret);
+
+        return Base32::decodeUpper($secret);
+    }
+
+        /**
+     * Extracts the OTP from the SHA1 hash.
+     *
+     * @param string $hash
+     *
+     * @return string
+     **/
+    private function oathTruncate(#[SensitiveParameter] string $hash): string
+    {
+        $offset = ord($hash[strlen($hash) - 1]) & 0xF;
+
+        $temp = unpack('N', substr($hash, $offset, 4));
+
+        $temp = $temp[1] & 0x7FFFFFFF;
+
+        return substr(
+            (string) $temp,
+            -$this->oneTimePasswordLength
+        );
+    }
+
+        /**
+     * Generate the HMAC OTP.
+     *
+     * @param string $secret
+     * @param int    $counter
+     *
+     * @return string
+     */
+    private function generateHotp(#[SensitiveParameter] string $secret, int $counter): string
+    {
+        return hash_hmac(
+            $this->algorithm->value,
+            pack('N*', 0, $counter), // Counter must be 64-bit int
+            $secret,
+            true
+        );
+    }
+}
--- src/SecretGenerator.php
+++ src/SecretGenerator.php
@@ -2,22 +2,24 @@
 
 declare(strict_types=1);
 
-namespace LiquidCats\G2FA\Support;
+namespace LiquidCats\G2FA;
 
-use LiquidCats\G2FA\Contracts\Support\Generator;
+use LiquidCats\G2FA\Contracts\Support\Validator;
 use LiquidCats\G2FA\Enums\Alphabet;
 use LiquidCats\G2FA\Exceptions\IncompatibleWithAuthenticatorException;
 use LiquidCats\G2FA\Exceptions\InvalidCharactersException;
 use LiquidCats\G2FA\Exceptions\SecretKeyTooShortException;
+use LiquidCats\G2FA\Support\SecretValidator;
 use ParagonIE\ConstantTime\Base32;
 use Random\RandomException;
 use SensitiveParameter;
 use function random_int;
 use function str_replace;
+use function var_dump;
 
-readonly class SecretGenerator implements Generator
+readonly class SecretGenerator
 {
-    public function __construct()
+    public function __construct(private Validator $validator = new SecretValidator())
     {
     }
 
@@ -27,6 +29,9 @@ readonly class SecretGenerator implements Generator
      *
      * @return string
      *
+     * @throws IncompatibleWithAuthenticatorException
+     * @throws InvalidCharactersException
+     * @throws SecretKeyTooShortException
      * @throws RandomException
      */
     public function secretKey(int $length = 16, #[SensitiveParameter] string $prefix = ''): string
@@ -35,7 +40,11 @@ readonly class SecretGenerator implements Generator
 
         $secret = str_replace('=', '', $secret);
 
-        return $this->strPadBase32($secret, $length);
+        $secret = $this->strPadBase32($secret, $length);
+
+        $this->validator->validate($secret);
+
+        return $secret;
     }
 
     /**
